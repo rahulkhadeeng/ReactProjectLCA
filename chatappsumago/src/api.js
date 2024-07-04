baf1826938f7d288d54763369e841c330eae6f2e
Rahul Khade <141446537+rahulkhadeeng@users.noreply.github.com>
2024-07-04 09:08:07 +0530

    Update api.js

--- chatappsumago/src/api.js
+++ chatappsumago/src/api.js
@@ -2,7 +2,7 @@
 const API_URL = process.env.PORT || 'https://reactprojectlca.onrender.com';
 
 export const fetchData = async () => {
-  const response = await fetch(`${https://reactprojectlca.onrender.com}/endpoint`);
+  const response = await fetch(`${API_URL}/endpoint`);
   const data = await response.json();
   return data;
 };
