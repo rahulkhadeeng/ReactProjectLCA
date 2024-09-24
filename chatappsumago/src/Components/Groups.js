8e9e03efedced1f6ba0626e2a1210246e6191f38
Rahul Khade <141446537+rahulkhadeeng@users.noreply.github.com>
2024-09-24 08:03:25 +0530

    Update Groups.js

--- chatappsumago/src/Components/Groups.js
+++ chatappsumago/src/Components/Groups.js
@@ -36,7 +36,7 @@ function Groups() {
     };
 
     axios
-      .get("http://localhost:5000/chat/fetchGroups", config)
+      .get("https://reactprojectlca.onrender.com/chat/fetchGroups", config)
       .then((response) => {
         console.log("Group Data from API ", response.data);
         SetGroups(response.data);
