e895bf189275fc53f5f9ae2b231c3e944dd2ff0c
Rahul Khade <141446537+rahulkhadeeng@users.noreply.github.com>
2024-09-24 09:07:44 +0530

    Update ChatArea.js

--- chatappsumago/src/Components/ChatArea.js
+++ chatappsumago/src/Components/ChatArea.js
@@ -78,7 +78,7 @@ import axios from "axios";
 import { myContext } from "./MainContainer";
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://reactprojectlca-frontend.onrender.com";
 
 var socket, chat;
  
