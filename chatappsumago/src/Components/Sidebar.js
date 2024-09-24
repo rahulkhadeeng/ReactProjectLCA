07a59284ad11be416916686fcaaad1dea040bf64
Rahul Khade <141446537+rahulkhadeeng@users.noreply.github.com>
2024-09-24 08:04:11 +0530

    Update Sidebar.js

--- chatappsumago/src/Components/Sidebar.js
+++ chatappsumago/src/Components/Sidebar.js
@@ -41,7 +41,7 @@ function Sidebar() {
       },
     };
 
-    axios.get("http://localhost:5000/chat/", config).then((response) => {
+    axios.get("https://reactprojectlca.onrender.com/chat/", config).then((response) => {
       console.log("Data refresh in sidebar ", response.data);
       setConversations(response.data);
       // setRefresh(!refresh);
